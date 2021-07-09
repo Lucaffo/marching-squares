2af190d0d1d37e7a9f1ba308d0e738a67b8dcdc2
Luca Raffo <55745404+Lucaffo@users.noreply.github.com>
2021-07-09 17:23:26 +0200

    Added the noise offset mover script
    + Added the noise offset mover script
    - Fixed the camera portrait to 1:1
    - Hided the spaceship.

---
+++ Marching Squares/Assets/Scripts/Character/NoiseOffsetMover.cs
@@ -0,0 +1,30 @@
+﻿using Procedural.Marching.Squares;
+using UnityEngine;
+
+namespace MarchingSquare.Utils
+{
+    public class NoiseOffsetMover : MonoBehaviour
+    {
+        [Header("Settings parameters")]
+        public float scrollSpeed = 1.0f;
+
+        // Scrolling voxel map
+        private VoxelMap map;
+
+        // Get the input direction
+        private Vector2 inputDirection;
+
+        private void Start()
+        {
+            map = FindObjectOfType<VoxelMap>();
+        }
+
+        private void Update()
+        {
+            inputDirection = Vector2.right * Input.GetAxis("Horizontal") + Vector2.up * Input.GetAxis("Vertical");
+            inputDirection.Normalize();
+
+            map.AddNoiseOffset(inputDirection * scrollSpeed);
+        }
+    }
+}
